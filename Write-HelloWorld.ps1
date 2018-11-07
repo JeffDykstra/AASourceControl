4d93bc391d27717306f68dc13ff50ae6ac0c078c
Jeff <jeffdyk@microsoft.com>
2018-11-07 11:34:43 -0600

    update2

--- Write-HelloWorld.ps1
+++ Write-HelloWorld.ps1
@@ -32,7 +32,7 @@ workflow Write-HelloWorld {
         [parameter(Mandatory=$false)]
         [String]$Name = "World"
     )
-        Write-verbose "test 123"
+        Write-verbose "test 12345"
         Write-Output "Hello $Name"
 
 }
